d624f66cff281208563e81a9bada2e68564caaf7
jesse <1152122933@qq.com>
2023-04-27 22:43:57 +1000

    combine the autoscaling and microservices

--- main.tf
+++ main.tf
@@ -1,42 +1,73 @@
 terraform {
-    required_providers { 
-        aws = { 
-            source = "hashicorp/aws" 
-            version = "~> 4.0" 
-        } 
-        docker = { 
-            source = "kreuzwerker/docker" 
-            version="3.0.2" 
-        } 
-    } 
-} 
-
-provider "aws" { 
-    region="us-east-1" 
-    shared_credentials_files = ["./credentials"] 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+  }
 }
 
-data "aws_ecr_authorization_token" "ecr_token"{} 
-
-provider "docker" { 
-    registry_auth { 
-        address = data.aws_ecr_authorization_token.ecr_token.proxy_endpoint 
-        username = data.aws_ecr_authorization_token.ecr_token.user_name 
-        password = data.aws_ecr_authorization_token.ecr_token.password 
-    } 
+provider "aws" {
+  region                   = "us-east-1"
+  shared_credentials_files = ["./credentials"]
+  default_tags {
+    tags = {
+      Course     = "CSSE6400"
+      Name       = "TaskOverflow"
+      Automation = "Terraform"
+    }
+  }
 }
 
-resource "aws_ecr_repository" "taskoverflow" { 
-    name = "taskoverflow"
+# used for database
+locals {
+  image             = docker_registry_image.taskoverflow.name
+  database_username = "administrator"
+  database_password = "VerySecurePassword123XYZ"
 }
 
-resource "docker_image" "taskoverflow" { 
-    name = "${aws_ecr_repository.taskoverflow.repository_url}:latest" 
-    build { 
-        context = "." 
-    } 
+# Docker
+data "aws_ecr_authorization_token" "ecr_token" {}
+
+provider "docker" {
+  registry_auth {
+    address  = data.aws_ecr_authorization_token.ecr_token.proxy_endpoint
+    username = data.aws_ecr_authorization_token.ecr_token.user_name
+    password = data.aws_ecr_authorization_token.ecr_token.password
+  }
 }
 
-resource "docker_registry_image" "taskoverflow" { 
-    name = docker_image.taskoverflow.name
+resource "aws_ecr_repository" "taskoverflow" {
+  name = "taskoverflow"
+}
+
+resource "docker_image" "taskoverflow" {
+  name = "${aws_ecr_repository.taskoverflow.repository_url}:latest"
+  build {
+    context = "."
+  }
+}
+
+resource "docker_registry_image" "taskoverflow" {
+  name = docker_image.taskoverflow.name
+}
+
+# add autoscale
+data "aws_iam_role" "lab" {
+  name = "LabRole"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
